64844ff6705f2fbab29b92a35a4648b85b7abe78
opee2012 <jborth2010@gmail.com>
2021-09-09 14:53:30 -0400

    Added Car Value labwork

---
+++ lab_09_08/Car.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include "Car.h"
+using namespace std;
+
+void Car::SetModelYear(int userYear){
+   modelYear = userYear;
+}
+
+int Car::GetModelYear() const {
+   return modelYear;
+}
+
+// TODO: Implement SetPurchasePrice() function
+
+// TODO: Implement GetPurchasePrice() function
+
+void Car::CalcCurrentValue(int currentYear) {
+   double depreciationRate = 0.15;
+   int carAge = currentYear - modelYear;
+      
+   // Car depreciation formula
+   currentValue = (int) 
+      round(purchasePrice * pow((1 - depreciationRate), carAge));
+}
+
+// TODO: Implement PrintInfo() function to output modelYear, purchasePrice, and 
+// currentValue
