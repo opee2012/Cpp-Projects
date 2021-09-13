874aa6005fe0230a65654df4857bbbd898a82d7b
opee2012 <jborth2010@gmail.com>
2021-09-13 12:48:58 -0400

    Add files to repo

---
+++ lab_09_13/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Triangle.h"
+using namespace std;
+
+int main() {
+   Triangle triangle1;
+   Triangle triangle2;
+
+   // TODO: Read and set base and height for triangle1 (use SetBase() and SetHeight())
+      
+   // TODO: Read and set base and height for triangle2 (use SetBase() and SetHeight())
+   
+   // TODO: Determine larger triangle (use GetArea())      
+   cout << "Triangle with larger area:" << endl;
+
+   // TODO: Output larger triangle's info (use PrintInfo())
+   
+   return 0;
+}
